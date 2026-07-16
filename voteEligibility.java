b6dfe3839cad90c052130ba4728638a7d67d0883
Karunasri Taviti <105840346+KarunasriTaviti@users.noreply.github.com>
2026-07-16 18:29:10 +0530

    Create voteEligibility.java to check voter eligibility
    Add a Java program to check voting eligibility based on age.

---
+++ voteEligibility.java
@@ -0,0 +1,13 @@
+//eligible to vote 
+class eligibility{
+    public static void main(String args[]){
+        int age=8;
+        if(age>=18){
+            System.out.println("Eligible to vote.");
+        }
+        else{
+            System.out.println("Not eligible to vote");
+        }
+        
+    }
+}
