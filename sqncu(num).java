cb4457c5e82593fef1f919fbba80ca3936ecc725
Karunasri Taviti <105840346+KarunasriTaviti@users.noreply.github.com>
2026-07-16 18:57:04 +0530

    Add square and cube calculation program
    Implement a program to calculate the square and cube of a number.

---
+++ sqncu(num).java
@@ -0,0 +1,7 @@
+//program to find the square and cube of a number
+class sqcu{
+    public static void main(String args[]){
+        int a=5;
+        System.out.println("Square: "+(a*a)+" Cube: "+(a*a*a));
+        }
+}
